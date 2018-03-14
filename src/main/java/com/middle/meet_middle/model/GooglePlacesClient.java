653a28b777f6bf276178600b467966c97e22733b
navsaini <nsaini@utexas.edu>
2018-03-14 09:58:40 -0500

    small change

--- src/main/java/com/middle/meet_middle/model/GooglePlacesClient.java
+++ src/main/java/com/middle/meet_middle/model/GooglePlacesClient.java
@@ -1,29 +1,20 @@
 package com.middle.meet_middle.model;
 
 import se.walkercrou.places.GooglePlaces;
-import se.walkercrou.places.Place;
-
-import java.util.List;
 
 public class GooglePlacesClient {
 
     private static GooglePlacesClient instance;
-    private GooglePlaces client;
+    public GooglePlaces client;
 
     private GooglePlacesClient() {
         client = new GooglePlaces(System.getenv("PLACES_KEY"));
     }
-
-    // TODO: implement google places methods in here as non-static
 
     public static GooglePlacesClient getInstance() {
         if (instance == null) {
             instance = new GooglePlacesClient();
         }
         return instance;
-    }
-
-    public List<Place> findPlaces(String query) {
-        return client.getPlacesByQuery(query);
     }
 }
--- src/main/java/com/middle/meet_middle/service/PlacesService.java
+++ src/main/java/com/middle/meet_middle/service/PlacesService.java
@@ -1,4 +1,17 @@
 package com.middle.meet_middle.service;
 
+import com.middle.meet_middle.model.GooglePlacesClient;
+import se.walkercrou.places.GooglePlaces;
+import se.walkercrou.places.Place;
+
+import java.util.List;
+
 public class PlacesService {
+    public GooglePlaces client = GooglePlacesClient.getInstance().client;
+
+    public List<Place> findPlacesByQuery(String query) {
+        return client.getPlacesByQuery(query);
+    }
+
+    // TODO: write findPlacesByCoordinates
 }
