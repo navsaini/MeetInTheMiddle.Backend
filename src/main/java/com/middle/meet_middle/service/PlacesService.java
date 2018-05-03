a9e60bae84ec5e959fe4b38515f6f6cdd5708855
navsaini <nsaini@utexas.edu>
2018-05-03 18:53:48 -0500

    tightened search criteria

--- src/main/java/com/middle/meet_middle/service/PlacesService.java
+++ src/main/java/com/middle/meet_middle/service/PlacesService.java
@@ -45,16 +45,22 @@ public class PlacesService {
         Point midPoint = EarthCalc.midPoint(loc1, loc2);
 
         List<Place> finalResult = new ArrayList<>();
-        for(String locType : poiTypes) {
+        for (String locType : poiTypes) {
             try {
                 finalResult.addAll(client.getNearbyPlacesRankedByDistance(midPoint.latitude, midPoint.longitude, limit, Param.name("keyword").value(locType)));
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("No places found");
             }
         }
 
-        return sortByDistance(finalResult, midPoint);
+        double threshold = calcDist(startLat, startLong, endLat, endLong);
+        return sortByDistance(finalResult, midPoint, threshold);
+    }
+
+    private double calcDist(double startLat, double startLong, double endLat, double endLong) {
+        Point loc1 = Point.at(Coordinate.fromDegrees(startLat), Coordinate.fromDegrees(startLong));
+        Point loc2 = Point.at(Coordinate.fromDegrees(endLat), Coordinate.fromDegrees(endLong));
+        return EarthCalc.gcdDistance(loc1, loc2);
     }
 
     public Point getMidpointCoords(double startLat, double startLong, double endLat, double endLong) {
@@ -66,14 +72,14 @@ public class PlacesService {
 
     /* Assumed 4 different location types: parks, bars, coffee shops, parks. */
     private int calculateLimit(String[] poiTypes) {
-        return MAX_RESULTS/poiTypes.length;
+        return MAX_RESULTS / poiTypes.length;
     }
 
-    private List<Place> sortByDistance(List<Place> finalResult, Point midPoint) {
+    private List<Place> sortByDistance(List<Place> finalResult, Point midPoint, double threshold) {
         List<Double> distance = new ArrayList<>();
         Map<Double, List<Place>> distanceMap;
 
-        for(Place place : finalResult) {
+        for (Place place : finalResult) {
             Point p = Point.at(Coordinate.fromDegrees(place.getLatitude()), Coordinate.fromDegrees(place.getLongitude()));
             distance.add(EarthCalc.gcdDistance(p, midPoint));
         }
@@ -81,7 +87,7 @@ public class PlacesService {
         distanceMap = generateDistancePlaceMap(finalResult, distance);
         Collections.sort(distance);
 
-        finalResult = sortResults(distance, distanceMap);
+        finalResult = sortResults(distance, distanceMap, threshold);
 
         return finalResult;
     }
@@ -90,13 +96,12 @@ public class PlacesService {
         Map<Double, List<Place>> distanceMap = new HashMap<>();
         int finalResultIndex = 0;
 
-        for(Double dist : distance) {
-            if(!distanceMap.containsKey(dist)) {
+        for (Double dist : distance) {
+            if (!distanceMap.containsKey(dist)) {
                 List<Place> newList = new ArrayList<>();
                 newList.add(finalResult.get(finalResultIndex));
                 distanceMap.put(dist, newList);
-            }
-            else {
+            } else {
                 List<Place> oldList = distanceMap.get(dist);
                 oldList.add(finalResult.get(finalResultIndex));
                 distanceMap.put(dist, oldList);
@@ -107,14 +112,16 @@ public class PlacesService {
         return distanceMap;
     }
 
-    private List<Place> sortResults(List<Double> distance, Map<Double, List<Place>> distanceMap) {
+    private List<Place> sortResults(List<Double> distance, Map<Double, List<Place>> distanceMap, double threshold) {
         List<Place> finalResult = new ArrayList<>();
 
-        for(Double dist : distance) {
+        for (Double dist : distance) {
             List<Place> placeAtDistance = distanceMap.get(dist);
 
-            for(Place place : placeAtDistance) {
-                finalResult.add(place);
+            if (dist <= threshold) {
+                for (Place place : placeAtDistance) {
+                    finalResult.add(place);
+                }
             }
         }
         return finalResult;
